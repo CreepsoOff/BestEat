28e2eb335478e6c6e4f7e8c1876c053bd1f77df8
CreepsoOff <fabian.gelin9@etu.univ-lorraine.fr>
2025-12-12 15:27:54 +0100

    update: REFONTE v1 de l'APP - réunion du code de tout le monde Jour 1 - Map - Images - Recherche - Profil - Favoris (preview)

--- BestEat/Screens/CarteVue.swift
+++ BestEat/Screens/CarteVue.swift
@@ -9,20 +9,49 @@ import SwiftUI
 import MapKit
 
 struct CarteVue: View {
+    
+    @State private var position: MapCameraPosition = .region(
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 50.6333, longitude: 3.0667),
+            span: MKCoordinateSpan(latitudeDelta: 0.04, longitudeDelta: 0.04)
+        )
+    )
+    
     var body: some View {
         NavigationStack {
-            Map {
+            Map(position: $position) {
                 
+                ForEach(restaurants) { restaurant in
+                    Annotation(restaurant.nom, coordinate: CLLocationCoordinate2D(latitude: restaurant.latitude, longitude: restaurant.longitude)) {
+                        VStack {
+                            Image(systemName: "fork.knife.circle.fill")
+                                .foregroundColor(.red)
+                                .font(.title2)
+                            
+                            Text(restaurant.nom)
+                                .font(.caption2)
+                                .fontWeight(.semibold)
+                                .padding(3)
+                                .background(Color.white.opacity(0.8))
+                                .cornerRadius(5)
+                        }
+                        .shadow(radius: 3)
+                    }
+                }
             }
+            .edgesIgnoringSafeArea(.all)
             .navigationBarTitle("")
-            .toolbar{
-                ToolbarItem(placement: .principal){
+            .toolbar {
+                ToolbarItem(placement: .principal) {
                     Text("Carte")
                         .font(.custom("Redaction-Regular", size: 32))
                 }
             }
+            .mapControls {
+                MapCompass()
+                MapPitchToggle()
+            }
         }
-        
     }
 }
 
